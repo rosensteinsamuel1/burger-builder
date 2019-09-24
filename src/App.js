73266d38b83795f5301e07a417656df812ef3726
rosenstein.samuel1 <rosenstein.samuel1@gmail.com>
2019-09-24 21:50:57 +0300

    inital commit, basic layout component implimented

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import styles from './App.module.css';
+import React, {Component} from 'react';
+import Layout from './components/Layout/Layout'
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;

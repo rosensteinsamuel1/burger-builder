a01782a6aafd3d04bf2a7643d16b89337d1051ef
rosenstein.samuel1 <rosenstein.samuel1@gmail.com>
2019-09-26 20:33:25 +0300

    added toolbar and navitems

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './Toolbar.module.css'
+import Logo from '../../Logo/Logo'
+import NavItems from '../NavItems/NavItems'
+
+const toolbar = (props) => (
+    <header className={classes.Toolbar}>
+        <div>Menu</div>
+        <Logo />
+        <nav>
+            <NavItems />
+        </nav>
+    </header>
+);
+
+export default toolbar;
